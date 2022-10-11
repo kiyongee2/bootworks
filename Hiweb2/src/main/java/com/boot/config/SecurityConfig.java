fe1f9a3a300e4beabcfead80543759c98aa7fc15
kiyongee2 <kiyongee2@gmail.com>
2022-10-11 14:13:57 +0900

    hiweb2 update

--- Hiweb2/src/main/java/com/boot/config/SecurityConfig.java
+++ Hiweb2/src/main/java/com/boot/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.boot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
